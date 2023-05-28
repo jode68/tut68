3f880bddee02e5075c79ee623afe993ff1274930
jode68 <xumepao@yahoo.it>
2023-05-28 22:01:40 +0200

    update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,38 +1,40 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:tut68/providers/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final Provider provider = Get.put(Provider());
+    void addItem() {
+      provider.lista.add('Paolo');
+    }
+
+    void logOut() {
+      FirebaseAuth.instance.signOut();
+      Get.back();
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
         leading: const Text(''),
         actions: [
-          IconButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-              Get.back();
-            },
-            icon: const Icon(Icons.logout),
-          ),
+          IconButton(onPressed: () => addItem(), icon: const Icon(Icons.add)),
+          IconButton(onPressed: () => logOut(), icon: const Icon(Icons.logout)),
         ],
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              color: Colors.lightBlue,
-              child: ListTile(
-                title: Center(child: Text(FirebaseAuth.instance.currentUser!.email.toString())),
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: Obx(() {
+        return ListView.builder(
+          itemCount: provider.lista.length,
+          itemBuilder: (context, index) {
+            return Text(provider.lista[index]);
+          },
+        );
+      }),
     );
   }
 }
---
+++ lib/providers/provider.dart
@@ -0,0 +1,5 @@
+import 'package:get/get.dart';
+
+class Provider extends GetxController {
+  List lista = [].obs;
+}
